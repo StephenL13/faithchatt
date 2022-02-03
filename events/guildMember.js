86509460bcbca28fdbfd49d752763e90ee5c84cc
Stephen L <67349718+StephenL13@users.noreply.github.com>
2022-02-03 10:10:35 +0000

    Update guildMember.js

--- events/guildMember.js
+++ events/guildMember.js
@@ -9,7 +9,7 @@ client.on('guildMemberUpdate', (oldMember, newMember) => {
         .setTitle(`Welcome to FaithChatt!`)
         .setDescription(`Be sure to check out our <#922188972854231160>  and review our <#843352934595166239> as you begin working with us. It gives you access to specific channels, a new color, and important reminders pings.\n\nYou may also want to review our <#839725174912057354> and share a little bit about yourself. We make it easy and even provide a template to follow. Thanks for joining.`)
         .setThumbnail(newMember.user.displayAvatarURL({ dynamic: true }))
-        .setFooter(`UID: ${newMember.user.id}`)
+        .setFooter({ text: `UID: ${newMember.user.id}` })
         .setTimestamp()
       client.channels.cache.get(`839722094798700555`).send({ content: `<@&912259335881650176>, ${newMember} has arrived!`, embeds: [welcome] });
     };
