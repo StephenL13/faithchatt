a417661ba3cb12a02d75d55108f816ff4e37ae22
unknown <luciajastephen@gmail.com>
2022-02-05 23:25:31 +0800

    Update bslock

--- commands/professor/bslock.js
+++ commands/professor/bslock.js
@@ -9,7 +9,7 @@ module.exports.run = async(client, message, args, prefix) => {
         .setFooter({ text: '© FaithChatt Forum' });
     const successEmbed = new MessageEmbed()
         .setColor('#ffd100')
-        .setDescription('<:Unstaged:880650957925519441> **Bible study channel is now locked.**\n\nTune in next time by [checking out our schedules](https://discord.com/channels/839708279973478430/839965374967054386/936968689608163388) on <#936968689608163388>. Thanks for joining with us. God bless you.')
+        .setDescription('<:Unstaged:880650957925519441> **Bible study channel is now locked.**\n\nTune in next time by [checking out our schedules](https://discord.com/channels/839708279973478430/839965374967054386/936968689608163388) on <#839965374967054386>. Thanks for joining with us. God bless you.')
         .setImage('https://media1.giphy.com/media/z1kL3TjrK278E6EhZF/giphy.gif?cid=790b7611e06601e976e82fa774ae08ceab4bd86a1f6672ea&rid=giphy.gif&ct=g')
         .setFooter({ text:"© FaithChatt Forum" });
     if(message.member.roles.cache.has({ professorRole, facilitatorRole }) || message.member.permissions.has("MANAGE_ROLES")) {
