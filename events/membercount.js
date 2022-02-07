3f6db49f18ef3d946ac0db4f751c68d3e591bcdc
unknown <luciajastephen@gmail.com>
2022-02-07 14:28:31 +0800

    Update membercount

--- events/membercount.js
+++ events/membercount.js
@@ -1,18 +1,20 @@
 const client = require('../index.js').client
 
-const faithchatt = client.guilds.cache.get('839708279973478430')
-setInterval(() => {
-    const humanMemberCount = faithchatt.members.cache.filter(member => !member.user.bot);
-    const humanMemberChannel = faithchatt.channels.cache.get('940128360347074600')
-    humanMemberChannel.setName(`👥 Members: ${humanMemberCount}`)
+client.on('ready', async() => {
+    const faithchatt = client.guilds.cache.get('839708279973478430')
+    setInterval(() => {
+        const humanMemberCount = faithchatt.members.cache.filter(member => !member.user.bot);
+        const humanMemberChannel = faithchatt.channels.cache.get('940128360347074600')
+        humanMemberChannel.setName(`👥 Members: ${humanMemberCount}`)
 
-    const boosterCount = faithchatt.members.cache.filter(member => member.roles.cache.has('852783343376269323'));
-    const boosterChannel = faithchatt.channels.cache.get('940128649292685382')
-    boosterChannel.setName(`🎆 Boosters: ${boosterCount}`)
+        const boosterCount = faithchatt.members.cache.filter(member => member.roles.cache.has('852783343376269323'));
+        const boosterChannel = faithchatt.channels.cache.get('940128649292685382')
+        boosterChannel.setName(`🎆 Boosters: ${boosterCount}`)
 
-    const gsCount = faithchatt.members.cache.filter(member => member.roles.cache.has('844788509301669898'));
-    const gsChannel = faithchatt.channels.cache.get('940128750723547157')
-    gsChannel.setName(`😇 Good Samaritans: ${gsCount}`)
+        const gsCount = faithchatt.members.cache.filter(member => member.roles.cache.has('844788509301669898'));
+        const gsChannel = faithchatt.channels.cache.get('940128750723547157')
+        gsChannel.setName(`😇 Good Samaritans: ${gsCount}`)
 
-    console.log('Member count is updating... (30 mins. interval)')
-}, 1000 * 60 * 30)
+        console.log('Member count is updating... (30 mins. interval)')
+    }, 1000 * 60 * 30)
+})
