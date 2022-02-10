c273788d1549197d602e74d744671c54b891a87d
unknown <luciajastephen@gmail.com>
2022-02-10 13:11:14 +0800

    Antiprofanity fix

--- events/antiprofanity.js
+++ events/antiprofanity.js
@@ -2,11 +2,14 @@ const client = require('../index.js').client
 
 client.on('messageCreate', async message => {
     // An array of detected profane words from Dyno (LORD FORGIVE ME LOL)
-    const profanity = ["anal","anus","arse","ass","bitch","b1tch","ballsack","bastard","biatch","blowjob","bollock","bullock","boner","boob","boobs","buttplug","clitoris","cock","cum","c0m","cunt","dick","d1ck","dildo","d1ldo","d1ld0","dild0","dlld0","dyke","erection","fag","faggot","feck","fellate","fellatio","felching","fuck","fucks","fudgepacker","genitals","jerk","jizz","knobend","labia","mastrubate","muff","nigger","nigga","penis","piss","poop","pube","pussy","scrotum","shit","sh1t","sh!t","slut","s1ut","smegma","smd","spunk","tit","titties","tities","tranny","trannies","tosser","turd","twat","vagina","wank","whore","tits","titty","asshole","fvck","asshat","pu55y","pen1s","nigg","fuk","cnut","b!tch","c0ck"]
+    const profanity = ["anal","anus","arse","ass","bitch","b1tch","ballsack","bastard","biatch","blowjob","bollock","bullock","boner","boob","boobs","buttplug","clitoris","cock","cum","c0m","cunt","damn","dick","d1ck","dildo","d1ldo","d1ld0","dild0","dlld0","dyke","erection","fag","faggot","feck","fellate","fellatio","felching","fuck","fucking","fucksake","fucks","fudgepacker","genitals","jerk","jizz","knobend","labia","mastrubate","muff","nigger","nigga","penis","piss","poop","pube","pussy","scrotum","shit","sh1t","sh!t","slut","s1ut","smegma","smd","spunk","tit","titties","tities","tranny","trannies","tosser","turd","twat","vagina","wank","whore","tits","titty","asshole","fvck","asshat","pu55y","pen1s","nigg","fuk","cnut","b!tch","c0ck"]
+    const symbol = [".", ",", "!", "?",]
     let foundInText = false
     for (let i in profanity) {
-        let 
         if(message.content.toLowerCase().includes(` ${profanity[i]} `)) foundInText = true
+        for (let i in symbol) {
+            if(message.content.toLowerCase().includes(` ${profanity[i]}`+symbol[i]+` `)) foundInText = true
+        }
     }
 
     if(foundInText) {
