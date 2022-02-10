a381dcc433109f3c119753c0836580b18dd7a107
unknown <luciajastephen@gmail.com>
2022-02-10 12:01:52 +0800

    Antiprofanity fix

--- events/antiprofanity.js
+++ events/antiprofanity.js
@@ -5,7 +5,7 @@ client.on('messageCreate', async message => {
     const profanity = ["anal","anus","arse","ass","bitch","b1tch","ballsack","bastard","biatch","blowjob","bollock","bullock","boner","boob","boobs","buttplug","clitoris","cock","cum","c0m","cunt","dick","d1ck","dildo","d1ldo","d1ld0","dild0","dlld0","dyke","erection","fag","faggot","feck","fellate","fellatio","felching","fuck","fucks","fudgepacker","genitals","jerk","jizz","knobend","labia","mastrubate","muff","nigger","nigga","penis","piss","poop","pube","pussy","scrotum","shit","sh1t","sh!t","slut","s1ut","smegma","smd","spunk","tit","titties","tities","tranny","trannies","tosser","turd","twat","vagina","wank","whore","tits","titty","asshole","fvck","asshat","pu55y","pen1s","nigg","fuk","cnut","b!tch","c0ck"]
     let foundInText = false
     for (let i in profanity) {
-        if(message.content.toLowerCase.includes(profanity[i])) foundInText = true
+        if(message.content.toLowerCase().includes(profanity[i])) foundInText = true
     }
 
     if(foundInText) {
