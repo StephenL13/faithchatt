ca5da1105c7916ad3a7f5b8a6a67067f1fd777ea
unknown <luciajastephen@gmail.com>
2022-02-12 01:32:39 +0800

    Fix askquestion.js

--- slashcommands/main/askquestion.js
+++ slashcommands/main/askquestion.js
@@ -1,10 +1,11 @@
+const { MessageEmbed } = requir('discord.js')
 const moment = require('moment')
 module.exports.run = async (client, interaction) => {
     const slashCmdString = interaction.options.getString("text")
     const textChannel = client.channels.cache.get("839724682134683649")
     let simpledate = await moment().format('l')
 
-    await interaction.reply({ content: "**Your question has been sent!**\n\n*NOTE: If this *"})
+    await interaction.reply({ content: "**Your question has been sent!**\n\n*NOTE: Should there be any submissions that is against the rules, it will be removed immediately.*"}).catch(e=>console.log(e))
     await textChannel.send({ embeds: [
         new MessageEmbed()
         .setColor('#ffd100')
