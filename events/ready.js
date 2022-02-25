84b1f9d27c3f32617654f1fbd41d223d51be2a7c
unknown <luciajastephen@gmail.com>
2022-02-26 02:27:10 +0800

    Update ready.js: add comment for createCmd

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,6 @@ client.on('ready', () => {
   client.user.setActivity('discord.gg/faithchatt', { type: "PLAYING" })
   console.log('The bot is ready!')
   
-  // initialize slash commands
+  // initialize slash commands; server ID: 839708279973478430
   createCmd(client, "839708279973478430")
 });
