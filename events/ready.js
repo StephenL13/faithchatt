ad68db323ecfba3e00720356a2fc548c103bbc28
unknown <luciajastephen@gmail.com>
2022-02-28 00:22:31 +0800

    Update changing status

--- events/ready.js
+++ events/ready.js
@@ -3,13 +3,13 @@ const { createCmd } = require('../dataHandler')
 client.on('ready', async () => {
   const humanCount = await client.guilds.cache.get('839708279973478430').members.cache.filter(member => !member.user.bot).size
   const arrayStatus = [
-    `${humanCount} members`, `${humanCount} members`, `${humanCount} members`,
+    `${humanCount} members`,
     `Sola Gratia`,
     `Sola Fide`,
     `Sola Scriptura`,
     `Solus Christus`,
     `Soli Deo Gloria`,
-    `discord.gg/faithchatt`, `discord.gg/faithchatt`, `discord.gg/faithchatt`,
+    `discord.gg/faithchatt`,
   ]
   let index = 0;
   setInterval(() => {
@@ -17,7 +17,7 @@ client.on('ready', async () => {
     const status = arrayStatus[index];
     client.user.setActivity(status, { type: "PLAYING" });
     index++;
-  }, 3000)
+  }, 5000)
   console.log('The bot is ready!')
   
   // initialize slash commands; server ID: 839708279973478430
