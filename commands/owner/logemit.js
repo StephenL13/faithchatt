d3b64312740d97da204b7b3a9384aacd21aaba76
unknown <luciajastephen@gmail.com>
2022-03-02 14:12:47 +0800

    Clear logemit

--- commands/owner/logemit.js
+++ commands/owner/logemit.js
@@ -4,26 +4,7 @@ const moment = require('moment')
 
 module.exports.run = async(client, message, args, prefix) => {
     if(message.author.id === "136292974379270144") {
-        let messageCollection = new Discord.Collection();
-        let channelMessages = await message.channel.messages.fetch({ limit: 100 }).catch(err => console.log(err));
-        messageCollection = await messageCollection.concat(channelMessages);
-        while (channelMessages.size === 100) {
-            let lastMessageId = await channelMessages.lastKey();
-            channelMessages = await message.channel.messages.fetch({ limit: 100, before: lastMessageId }).catch(err => console.log(err));
-            if (channelMessages) {
-                messageCollection = await messageCollection.concat(channelMessages);
-            };
-        };
-        let msgs = await messageCollection.filter(msg => !msg.length).reverse();
-        const text = await msgs.map(m=>`${m.author.tag}: ${m.content}`).join("\n")
-
-        if(text.length >= 2000) {
-            const timestamp = await moment().format("M-D-YYYY, HH:mm")
-            const fileAttach = new MessageAttachment(Buffer.from(text), `VerifyLog - ${timestamp}.txt`)
-            await message.channel.send({ files: [fileAttach] })
-        } else {
-            await message.channel.send({ content: `\`\`\`\n${text}\`\`\``})
-        }
+        
     } else return message.delete()
 }
 
