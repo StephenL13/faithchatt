aa9b97784ca9f2cf5ffeb7c895e26cbec13f7a73
Stephen Luciaja <luciajastephen@gmail.com>
2022-03-12 18:41:37 +0800

    Update modsuggest

--- commands/moderator/modsuggest.js
+++ commands/moderator/modsuggest.js
@@ -3,26 +3,31 @@ const { MessageEmbed } = require('discord.js')
 
 module.exports.run = async (client, message, args, prefix) => {
     const staffPermCheck = message.member.permissions.has("MANAGE_ROLES") && message.member.roles.cache.has(faithchatt.rolesId.staff)
+    const textChannel = client.channels.cache.get(faithchatt.textId.suggest)
+    const suggestion = args.join(" ")
 
-    if(!staffPermCheck) message.delete().then(async() => {
+    if(!staffPermCheck) return message.delete().then(async() => {
         await message.author.send({ content: "You are not a staff member allowed to use this command." })
     }).catch(e => {})
-
-    const textChannel = client.channels.cache.get(faithchatt.textId.suggest)
-
+    if(!suggestion) return message.reply({ content: "Please enter your query."}).then(async(m) => {
+        setTimeout(async() => {
+            await m.delete()
+        }, 5000)
+    }).catch(e => {})
     await message.delete();
     await message.author.send({ content: "**Your suggestion has been sent!**", ephemeral: true }).catch(e=>console.log(e))
     await textChannel.send({ embeds: [
         new MessageEmbed()
         .setColor('#ffd100')
-        .setAuthor({ name: `${interaction.user.tag} suggests:`, iconURL: interaction.user.displayAvatarURL() })
-        .setDescription(slashCmdString)
+        .setAuthor({ name: `${message.author.tag} suggests:`, iconURL: message.author.displayAvatarURL() })
+        .setDescription(suggestion)
         .setFooter({ text: "© FaithChatt Forum" })
     ] }).then(m => {
         m.react("<:thumb_green:918899980423544843>")
         m.react("<:thumb_red:918899980377411634>")
         m.react("❔")
     })
+    
 }
 
 module.exports.command = {
