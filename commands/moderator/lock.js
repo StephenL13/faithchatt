1947ee819f16cbd48f4bd498e6d289d126173964
Stephen Luciaja <luciajastephen@gmail.com>
2022-03-13 00:26:29 +0800

    Update lock/unlock

--- commands/moderator/lock.js
+++ commands/moderator/lock.js
@@ -2,6 +2,9 @@ const { textId, rolesId } = require('../../variablehandler.js')
 const { MessageEmbed } = require('discord.js')
 
 module.exports.run = async (client, message, args, prefix) => {
+    const memberrole = message.member.roles.cache.get(rolesId.member)
+    const regularrole = message.member.roles.cache.get(rolesId.regular)
+    const muted = message.member.roles.cache.get(rolesId.muted)
     let moderator = message.member.roles.cache.has(rolesId.staff)
     if(message.channel.id === textId.biblestudy) return message.delete().catch(e=>{})
     if(message.member.permissions.has("MANAGE_ROLES")) {
@@ -9,21 +12,17 @@ module.exports.run = async (client, message, args, prefix) => {
         const lockEmbed = new MessageEmbed()
           .setColor("#FF0000")
           .setTitle("🔒 Channel is locked.")
-          .setDescription(
-            "**You are not muted.**\nIt is currently on lockdown, please wait until the staff unlocks this channel."
-          )
+          .setDescription("**You are not muted.**\nIt is currently on lockdown, please wait until the staff unlocks this channel.")
           .setFooter({ text: "© FaithChatt Forum" });
-        message.channel.permissionOverwrites.edit(message.guild.id, {
-          SEND_MESSAGES: false,
-        });
-        message.channel.permissionOverwrites.edit();
-        message.channel.permissionOverwrites.edit();
-        message.channel.permissionOverwrites.edit();
-        message.channel.permissionOverwrites.edit();
-        message.channel.send({ embeds: [lockEmbed] });
+        await message.channel.permissionOverwrites.edit(message.guild.id, { SEND_MESSAGES: false });
+        await message.channel.permissionOverwrites.edit(moderatorRole.id, { SEND_MESSAGES: true });
+        await message.channel.permissionOverwrites.edit(memberrole.id, { SEND_MESSAGES: false });
+        await message.channel.permissionOverwrites.edit(regularrole.id, { SEND_MESSAGES: false });
+        await message.channel.permissionOverwrites.edit(muted.id, { SEND_MESSAGES: false });
+        await message.channel.send({ embeds: [lockEmbed] });
       } else {
-        message.delete();
-        message.author.send(
+        await message.delete();
+        await message.author.send(
           { content:"You are not a staff member authorized to use this command." }
         );
       }
--- commands/moderator/unlock.js
+++ commands/moderator/unlock.js
@@ -5,23 +5,20 @@ module.exports.run = async (client, message, args, prefix) => {
   if(message.channel.id === textId.biblestudy) return message.delete().catch(e=>{})
   if(message.member.permissions.has("MANAGE_ROLES")){
     if (message.member.roles.cache.has(rolesId.staff)) {
+      const moderatorRole = message.member.roles.cache.get(rolesId.staff)
       const lockEmbed = new MessageEmbed()
         .setColor("#FF0000")
         .setTitle("🔐 Channel is unlocked.")
         .setFooter({ text: "© FaithChatt Forum" });
-        message.channel.permissionOverwrites.edit(message.guild.id, {
-          SEND_MESSAGES: true,
-        });
-        message.channel.permissionOverwrites.edit(
-          message.guild.roles.cache.get("871058889339207681"),
-          {
-            SEND_MESSAGES: null,
-          }
-        );
-      message.channel.send({ embeds: [lockEmbed] });
+      await message.channel.permissionOverwrites.edit(message.guild.id, { SEND_MESSAGES: null });
+      await message.channel.permissionOverwrites.edit(moderatorRole.id, { SEND_MESSAGES: true });
+      await message.channel.permissionOverwrites.edit(memberrole.id, { SEND_MESSAGES: true });
+      await message.channel.permissionOverwrites.edit(regularrole.id, { SEND_MESSAGES: null });
+      await message.channel.permissionOverwrites.edit(muted.id, { SEND_MESSAGES: false });
+      await message.channel.send({ embeds: [lockEmbed] });
     } else {
-      message.delete();
-      message.author.send("You are not a staff member authorized to use this command.");
+      await message.delete();
+      await message.author.send("You are not a staff member authorized to use this command.");
     }
   }
 };
