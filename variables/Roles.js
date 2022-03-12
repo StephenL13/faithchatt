b1bc128df0070e5076ca04a904a00f866fb05461
Stephen Luciaja <luciajastephen@gmail.com>
2022-03-13 01:24:09 +0800

    Roles Variables: remove pending; add legal age

--- variables/Roles.js
+++ variables/Roles.js
@@ -1,14 +1,14 @@
-// UNVERIFIED MEMBER
+// BASE MEMBER ROLE
 var unverified = "940052640472109117"
-var pending = "940281435644911656"
-
-// BASE MEMBER
 var member = "839720518213959701"
 var regular = "839720156065169449"
 var muted = "859912959660785667"
+var underage = "849442918941851678"
+var legal = "849442852785487893"
 
 // MISCELLANEOUS
 var prayerwarrior = "844787634030772224"
+var usher = "927436319318224906"
 var welcomeping = "912259335881650176"
 var booster = "852783343376269323"
 var divPing = "908240970523492353"
@@ -20,8 +20,8 @@ var professor = "843151409167073350"
 var facilitator = "903033680497365033"
 
 module.exports = {
-    unverified, pending,
-    member, regular, muted, divPing, divProfile,
-    prayerwarrior, welcomeping, booster,
-    staff, professor, facilitator
+    unverified, member, regular, muted,
+    divPing, divProfile, prayerwarrior, usher, 
+    welcomeping, booster, underage, legal,
+    staff, professor, facilitator 
 }
