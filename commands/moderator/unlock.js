36493526c00f5c83fb08697a29549e844710a6ec
Stephen L <luciajastephen@gmail.com>
2022-04-07 11:08:09 +0800

    lock/unlock exclusive to mods

--- commands/moderator/unlock.js
+++ commands/moderator/unlock.js
@@ -6,7 +6,7 @@ module.exports.run = async (client, message, args, prefix) => {
   const regularrole = message.guild.roles.cache.get(rolesId.regular)
   const muted = message.guild.roles.cache.get(rolesId.muted)
   const moderatorCheck = message.member.roles.cache.has(rolesId.staff, rolesId.moderator)
-  const moderatorRole = message.guild.roles.cache.get(rolesId.staff)
+  const moderatorRole = message.guild.roles.cache.get(rolesId.moderator)
   if(message.channel.id === textId.biblestudy) return message.delete().catch(e=>{})
   if(!moderatorCheck) return message.delete().then(()=>{
     try {
