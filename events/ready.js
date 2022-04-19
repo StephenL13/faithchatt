fd89b484c6ac3e62fecafbdf44ac084617460fd8
Stephen Luciaja <luciajastephen@gmail.com>
2022-04-19 20:39:15 +0800

    Add error logging when bot fails to start

--- events/ready.js
+++ events/ready.js
@@ -23,3 +23,13 @@ client.on('ready', async () => {
   // initialize slash commands; server ID: 839708279973478430
   createCmd(client, "839708279973478430")
 });
+
+// ADD ERROR LOGGING WHEN BOT FAILS TO START
+
+client.on('shardError', (error) => {
+  console.error(error)
+})
+
+client.on('unhandledRejection', error => {
+  console.log('Test error:', error);
+});
