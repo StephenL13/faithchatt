8f61fb8fcd22aeeb6de0f9d04ad16d160fc91931
Stephen Luciaja <luciajastephen@gmail.com>
2022-04-21 21:31:34 +0800

    Modnick: Fix async/await

--- commands/moderator/modnick.js
+++ commands/moderator/modnick.js
@@ -24,7 +24,7 @@ module.exports.run = async(client, message, args, prefix) => {
                     .setDescription("❌ | You must mention a user to change their nickname.")
                     .setColor("#ff0000")
                 ]})
-                await setTimeout(() => message.delete().catch(() => {}), 5000)
+                setTimeout(() => message.delete().catch(() => {}), 5000)
             } catch (error) {
                 console.log(error)
             }
