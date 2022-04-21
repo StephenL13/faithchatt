895626850e88b0857922da88588a0bdfa397ff17
Stephen Luciaja <luciajastephen@gmail.com>
2022-04-21 21:26:49 +0800

    New modnick

---
+++ commands/moderator/modnick.js
@@ -0,0 +1,53 @@
+const { MessageEmbed } = require('discord.js')
+const { nicknameSet } = require(`../../variables/MiscValues.js`)
+module.exports.run = async(client, message, args, prefix) => {
+    let targetmember = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+    var num1 = Math.floor(Math.random()*nicknameSet.length);
+    var num2 = Math.floor(Math.random()*nicknameSet.length);
+    const modCheck = message.member.permissions.has("MANAGE_NICKNAMES")
+    if(!modCheck) return message.delete().then(() => {
+        try {
+            message.author.send({ embeds: [
+                new MessageEmbed()
+                .setDescription("❌ | You are not a staff member authorized to use this command.")
+                .setColor("#ff0000")
+            ]})
+        } catch (error) {
+            console.log(error)
+        }
+    })
+    if(!targetmember) {
+        return message.delete().then(() => {
+            try {
+                message.channel.send({ embeds: [
+                    new MessageEmbed()
+                    .setDescription("❌ | You must mention a user to change their nickname.")
+                    .setColor("#ff0000")
+                ]}).then(async() => {
+                    await setTimeout(() => message.delete(), 5000).catch(() => {})
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        })
+    } else {
+        await targetmember.setNickname(nicknameSet[num1]+nicknameSet[num2])
+        return message.delete().then(() => {
+            try {
+                await message.react("✅")
+                message.channel.send({ embeds: [
+                    new MessageEmbed()
+                    .setDescription(`✅ | ${targetmember.user.tag}'s nickname has been generated and changed!`)
+                    .setColor("#00ff00")
+                ]})
+            } catch (error) {
+                console.log(error)
+            }
+        })
+    }
+}
+
+module.exports.command = {
+    name: "modnick",
+    aliases: ["mnick"]
+}
--- variables/MiscValues.js
+++ variables/MiscValues.js
@@ -17,7 +17,14 @@ const questions = [
     "Is having faith enough to save us?",
     "Can you become a Christian after you die?",
     "Is there any sin God will not forgive?",
-    "Did Jesus ever sin?"
+    "Did Jesus ever sin?",
+    "What is worship?",
+    "How much do you spend time with God daily?",
+    "How do we judge everything according to Scriptures?",
 ]
 
-module.exports = { questions }
+const nicknameSet = [
+    "Boo", "Mouse", "Munchkin", "Bee", "Dolly", "Precious", "Bug", "Chipmunk", "Dottie", "CutiePie", "BonnyLass", "Sweetums", "Toots", "Buttercup", "Lovey", "Nugget", "Teacup", "Oldie", "Shortie", "Kiddo", "Smarty", "Boomer", "Scout", "Ace", "Goon", "Punk", "Rambo", "Gump", "Bond", "Giggles", "Speedy", "Squirt", "Smiley", "Rapunzel", "Ms.Congeniality", "Teeny", "Buddy", "King", "Champ", "Bro", "Amigo", "Bubba", "Tank", "Tiny", "Sport", "Slim", "Chief", "Buck", "Coach", "Junior", "Senior", "Doc", "Dude", "Pal", "Buster", "Bud", "Bestie", "BFF", "Queen", "Senorita", "Girly", "Gal", "FriendforLife", "ForeverFriend", "SoulSister", "Sis", "Chica", "Missy", "Rideordie", "Homegirl", "Homeboy", "Chain", "Smudge", "Iron", "Anvil", "Dirt", "Steel", "Hammer", "Blue-collar", "Titanium", "Blacksmith", "OilChange", "Plastic", "Factory", "TheMechanic", "Rubber", "TheIndustrial", "Metal", "Nylon", "Foundry", "Coal", "ShrinkWrap", "Soot", "Charcoal", "Concrete", "Oil", "Grill", "Chainlink", "Grease", "Wire", "Drywall", "Braniac", "Maestro", "ShireBait", "JazzMan", "Fiero", "MountDoom", "Lego", "FirstChair", "Gandalf", "Muggle", "SecondChair", "Saruman", "Numbers", "Engineer", "Faramir", "Algebra", "Bridge", "Boromir", "Calculus", "Architect", "Geo", "Photon", "Weasley", "Genius", "Squirt", "Brutus", "Chief", "Buster", "Buds", "Romeo", "Diesel", "Champ", "Kid", "Gizmo", "Sport", "Bandit", "Scout", "Slick", "Rocco", "Thor", "Cowboy", "Boomer", "Ziggy", "Slim", "Ace", "Vader", "Einstein", "Proton", "Tesla", "Neutron", "Enigma", "Electron", "Buckbeak", "Smarty", "Atom", "Valedictorian", "Newton", "Dragon", "ClassPrez", "Reactor", "Dumbledore", "Albus", "Gillyweed", "MusicMan", "Sauron", "Goblin", "Aragorn", "Sync", "Buckey", "Impro", "Silmaril", "Buckeye", "Bandman", "Elf", "Conductor", "Halfling", "Fellow"
+]
+
+module.exports = { questions, nicknameSet }
