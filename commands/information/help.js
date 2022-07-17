501ddba7e62ec953d308a8a8f703ce836c0d31fc
StephenL13 <luciajastephen@gmail.com>
2022-07-17 12:36:38 +0800

    Update help cmd

--- commands/information/help.js
+++ commands/information/help.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args, prefix) => {
   const embed = new MessageEmbed()
     .setColor("#ffd100")
     .setTitle("The setlist of commands")
-    .setDescription("**GENERAL USE**\n`!help` - This list\n`!ping` - Server ping\n`!invite` - Server invite\n\n**APPLICATION FORMS**\n`!moderatorform` `!adminform`\n`!teacherform` `!partnerform`\n\n**SLASH COMMANDS**\n`/pray` - Submit a prayer request")
+    .setDescription("**GENERAL USE**\n`!help` - This list\n`!ping` - Server ping\n`!membercount` - Human member count\n`!invite` - Server invite\n\n**APPLICATION FORMS**\n`!moderatorform` `!adminform`\n`!teacherform` `!partnerform`\n\n**SLASH COMMANDS**\n`/membercount` - Human member count\n`/pray` - Submit a prayer request")
     .setFooter({ text:"© FaithChatt Forum" })
   message.channel.send({ embeds: [embed] });
 };
--- slashcommands/info/help.js
+++ slashcommands/info/help.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, interaction) => {
         new MessageEmbed()
         .setColor("#ffd100")
         .setTitle("The setlist of commands")
-        .setDescription("**GENERAL USE**\n`!help` - This list\n`!ping` - Server ping\n`!invite` - Server invite\n\n**APPLICATION FORMS**\n`!moderatorform` `!adminform`\n`!teacherform` `!partnerform`\n\n**SLASH COMMANDS**\n`/pray` - Submit a prayer request")
+        .setDescription("**GENERAL USE**\n`!help` - This list\n`!ping` - Server ping\n`!membercount` - Human member count\n`!invite` - Server invite\n\n**APPLICATION FORMS**\n`!moderatorform` `!adminform`\n`!teacherform` `!partnerform`\n\n**SLASH COMMANDS**\n`/membercount` - Human member count\n`/pray` - Submit a prayer request")
         .setFooter({ text:"© FaithChatt Forum" })
     ], ephemeral: true })
 }
