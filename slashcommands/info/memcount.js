72f974391e3332baf3350d376f652ecc1a369bca
StephenL13 <luciajastephen@gmail.com>
2022-07-17 12:36:26 +0800

    Fix MessageEmbed

--- slashcommands/info/memcount.js
+++ slashcommands/info/memcount.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require('discord.js')
 module.exports.run = async(client, interaction) => {
     const faithchatt = client.guilds.cache.get('839708279973478430')
     const humanMemberCount = faithchatt.members.cache.filter(member => !member.user.bot).size;
