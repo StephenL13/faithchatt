13676a4dfa5f9ae58f288207c5b67a056fb6588c
StephenL13 <luciajastephen@gmail.com>
2022-07-17 12:33:33 +0800

    Member count prefix cmd update

--- commands/information/memcount.js
+++ commands/information/memcount.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require('discord,js')
+const { MessageEmbed } = require('discord.js')
 module.exports.run = async(client, message, args, prefix) => {
     const faithchatt = client.guilds.cache.get('839708279973478430')
     const humanMemberCount = faithchatt.members.cache.filter(member => !member.user.bot).size;
