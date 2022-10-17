1298206bedbe4527a10a8d40380151aae8caecf0
Stephen L <67349718+StephenL13@users.noreply.github.com>
2022-10-17 16:15:33 +0800

    Ask Questions system abolished

--- model/aqblacklist.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema = new mongoose.Schema({
-    userId: { type: String },
-    userName: { type: String }
-});
-
-module.exports = mongoose.model('aq-blacklist', schema)
